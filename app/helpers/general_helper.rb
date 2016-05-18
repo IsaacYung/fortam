d47b9004e5db221cd665f67ce9668250350c5778
IsaacYung <isaaczoi@gmail.com>
2016-05-18 00:41:18 -0300

    Add Billboard

--- app/helpers/general_helper.rb
+++ app/helpers/general_helper.rb
@@ -1,2 +1,11 @@
 module GeneralHelper
+  def menu(ul_class, div_class)
+    list_menu = content_tag(:ul, class: ul_class) do
+      @menu.collect do |option|
+        concat content_tag(:li, option)
+      end
+    end
+
+    content_tag(:div, list_menu, class: div_class)
+  end
 end
