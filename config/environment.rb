68995f3f4cfb040e0996bfc79edb74e0aacee0e2
IsaacYung <isaaczoi@gmail.com>
2016-05-29 00:25:57 -0300

    Add smtp server

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.gmail.com',
+  :domain         => 'mail.google.com',
+  :port           => 587,
+  :user_name      => 'isaaczoi@gmail.com',
+  :password       => 'Ogml15Lopi*08',
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.raise_delivery_errors = true
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_529_015_457) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 20160529015457) do
 
-  create_table 'messages', force: :cascade do |t|
-    t.string   'name'
-    t.string   'email'
-    t.string   'message'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
