49ea1f6a26610dfcb06bac624914e407e76f7b15
IsaacYung <isaaczoi@gmail.com>
2016-06-05 01:52:00 -0300

    Fix error

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,14 +1,11 @@
 class GeneralController < ApplicationController
   def index
-    expires_in 10.minutes, :public => true
   end
 
   def contact
     @message = Message.new
-    expires_in 10.minutes, :public => true
   end
 
   def about
-    expires_in 10.minutes, :public => true
   end
 end
